4aa80816f5ec8b03f4461f01fc0235838fddfc4c
joao <joao182cx@gmail.com>
2019-01-30 11:10:12 -0200

    KeyAdapter adicionado ao fieldLinhas.

--- src/ProtetorDeTelaApp.java
+++ src/ProtetorDeTelaApp.java
@@ -6,9 +6,6 @@ public class ProtetorDeTelaApp {
 		
 		ProtetorDeTelaFrame frame = new ProtetorDeTelaFrame();	
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		ProtetorDeTela prot = new ProtetorDeTela();
-		frame.add(prot);
 		frame.setVisible(true);
 		frame.setSize(500, 500);
 	}
--- src/ProtetorDeTelaFrame.java
+++ src/ProtetorDeTelaFrame.java
@@ -1,5 +1,7 @@
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -18,12 +20,26 @@ public class ProtetorDeTelaFrame extends JFrame{
 		prot = new ProtetorDeTela();
 		
 		fieldLinhas = new JTextField(5);
+		fieldLinhas.setText("4");
 		
+		fieldLinhas.addKeyListener(new LimiteListener());
 		
 		panelLinhas.add(fieldLinhas);
 		
 		add(panelLinhas, BorderLayout.NORTH);
 		add(prot, BorderLayout.CENTER);
+		
+	}
+	
+	private class LimiteListener extends KeyAdapter{
+
+		@Override
+		public void keyReleased(KeyEvent arg0) {
+			
+			System.out.println(fieldLinhas.getText());
+			
+		}
+
 		
 	}
 	
